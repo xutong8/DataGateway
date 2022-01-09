632117a3024cfffbd2ec7d2bdedaafbe39c9e6bc
Tong XU <2451836009@qq.com>
2022-01-09 11:33:04 +0800

    feat: divide three threads

--- src/main/java/com/zjuvai/oppo/config/ScheduleTask.java
+++ src/main/java/com/zjuvai/oppo/config/ScheduleTask.java
@@ -22,13 +22,40 @@ public class ScheduleTask {
     private static final Logger logger = LoggerFactory.getLogger(ScheduleTask.class);
 
     @Scheduled(fixedRate = 100)
-    public void mockPersonData() {
+    public void mockPersonData_thread1() {
         Date date = new Date();
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-        for (int i = 0; i < 10000; i++) {
-            String sql = "insert into person select 'yxy" + i + "', 21, '" + dateFormat.format(date) + "'";
-            logger.info("execute insert sql: " + sql);
-            jdbcTemplate.execute(sql);
-        }
+        String sql = "insert into person select 'yxy', 21, '" + dateFormat.format(date) + "'";
+        logger.info("thread1 execute insert sql: " + sql);
+        jdbcTemplate.execute(sql);
+    }
+
+    @Scheduled(fixedRate = 100)
+    public void mockPersonData_thread2() {
+        Date date = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        String sql = "insert into person select 'yxy', 21, '" + dateFormat.format(date) + "'";
+        logger.info("thread2 execute insert sql: " + sql);
+        jdbcTemplate.execute(sql);
+    }
+
+    /**
+     * Date date = new Date();
+     * SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+     * for (int i = 0; i < 10000; i++) {
+     * String sql = "insert into person select 'yxy" + i + "', 21, '" +
+     * dateFormat.format(date) + "'";
+     * logger.info("thread3 execute insert sql: " + sql);
+     * jdbcTemplate.execute(sql);
+     * }
+     */
+
+    @Scheduled(fixedRate = 100)
+    public void mockPersonData_thread3() {
+        Date date = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        String sql = "insert into person select 'yxy', 21, '" + dateFormat.format(date) + "'";
+        logger.info("thread3 execute insert sql: " + sql);
+        jdbcTemplate.execute(sql);
     }
 }
